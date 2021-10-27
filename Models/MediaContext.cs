95e32b419f560a697d6ea1f803fe2085d0d1809b
Matt Nev <mattnev@microsoft.com>
2021-10-27 15:40:09 -0500

    add initial files

---
+++ Models/MediaContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaAPI.Models
+{
+    public class MediaContext : DbContext
+    {
+        public MediaContext(DbContextOptions<MediaContext> opt) : base(opt) { }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
